b6fdf7e097e1c3a36c59e810ba1dc12565903241
AKHILA-AKHI02 <vyshnavaakhila123@gmail.com>
2025-06-27 21:19:12 +0530

    Add files via upload

---
+++ trng/phase1/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def day1(request):
+    return HttpResponse('Hello, Akhila')
+def day2(request):
+    return HttpResponse('Hello, This is Day2')
+def day3(request):
+    return HttpResponse('Hello, This is Day3')
+def day4(request):
+    return HttpResponse('Hello,  This is Day4')
+def day5(request):
+    return HttpResponse('Hello, This is Day5')
+def day6(request):
+    return HttpResponse('Hello, This is Day6')
+def day7(request):
+    return HttpResponse('Hello, This is Day7')
+def day8(request):
+    return HttpResponse('Hello, This is Day8')
+def day9(request):
+    return HttpResponse('Hello, This is Day9')
+def day10(request):
+    return HttpResponse('Hello, This is Day10')
+def day11(request):
+    return HttpResponse('Hello, This is Day11')
+def day12(request):
+    return HttpResponse('Hello, This is Day12')
+def day13(request):
+    return HttpResponse('Hello, This is Day13')
+def day14(request):
+    return HttpResponse('Hello, This is Day14')
+def day15(request):
+    return HttpResponse('Hello, This is Day15')
+
+
+# Create your views here.
---
+++ trng/phase2/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def day16(request):
+    return HttpResponse("Hello,Day16")
+def day17(request):
+    return HttpResponse("Hello,Day17")
+def day18(request):
+    return HttpResponse("Hello,Day18")
+def day19(request):
+    return HttpResponse("Hello,Day19")
+def day20(request):
+    return HttpResponse("Hello,Day20")
+def day21(request):
+    return HttpResponse("Hello,Day21")
+def day22(request):
+    return HttpResponse("Hello,Day22")
+def day23(request):
+    return HttpResponse("Hello,Day23")
+def day24(request):
+    return HttpResponse("Hello,Day24")
+def day25(request):
+    return HttpResponse("Hello,Day25")
+def day26(request):
+    return HttpResponse("Hello,Day26")
+def day27(request):
+    return HttpResponse("Hello,Day27")
+def day28(request):
+    return HttpResponse("Hello,Day28")
+def day29(request):
+    return HttpResponse("Hello,Day29")
+def day30(request):
+    return HttpResponse("Hello,Day30")
+
+
+
+
+
+
+
+
+
+
+
+# Create your views here.
