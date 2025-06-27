2a6b55d36910e5160176f058eec2af69890dce3a
AKHILA-AKHI02 <vyshnavaakhila123@gmail.com>
2025-06-27 21:21:00 +0530

    Add files via upload

---
+++ trngtemplate/phase3/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def akhila(request):
+    return HttpResponse('This is  created by Akhila ,Iam pursuing MCA 1st year 2nd SEMISTER') 
+    
+
+
+
+
+# Create your views here.
